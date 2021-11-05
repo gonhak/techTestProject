7b56b76b7d6bce4fd9b9d3e21a4fd445e14993f2
Filip Natanek <natanek291@gmail.com>
2021-11-05 20:14:38 +0100

    first structure of the project

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+const {
+    src,
+    dest,
+    series,
+    parallel,
+    watch
+} = require('gulp');
+
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const clean = require('gulp-clean');
+const kit = require('gulp-kit');
+
+const paths = {
+    sass: "./src/sass/**/*.scss",
+    sassDest: "./dist/css",
+
+    js: "./src/js/**/*.js",
+    jsDest: "./dist/js",
+
+    img: "./src/img/**/*",
+    imgDest: "./dist/img",
+
+    dist: "./dist",
+    html: "./html/**/*.kit"
+};
+
+function sassCompiler(done) {
+    src(paths.sass)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(dest(paths.sassDest));
+    done();
+};
+
+function javascript(done) {
+    src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(dest(paths.jsDest));
+    done();
+};
+
+function convertImages(done) {
+    src(paths.img)
+        .pipe(imagemin())
+        .pipe(dest(paths.imgDest));
+    done();
+};
+
+function handleKits(done) {
+    src(paths.html)
+        .pipe(kit())
+        .pipe(dest('./'));
+    done();
+};
+
+function cleanStuff(done) {
+    src(paths.dist, {read: false})
+        .pipe(clean())
+    done();
+};
+
+function startBrowserSync(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    done();
+};
+
+function watchForChanges(done) {
+    
+    watch('./*.html').on("change", reload);
+    watch([paths.sass, paths.html, paths.js], parallel(handleKits, sassCompiler, javascript)).on("change", reload);
+    watch(paths.img, convertImages).on("change", reload);;                                               
+    done();
+};
+
+const mainFunctions = parallel(handleKits, sassCompiler, javascript, convertImages);
+exports.cleanStuff = cleanStuff;
+exports.default = series(mainFunctions, startBrowserSync, watchForChanges);
